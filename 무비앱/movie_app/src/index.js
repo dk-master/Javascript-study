666b6ba2379542f2c8fc201f9dfe6bfd281e5c33
dk-master <sunilair@naver.com>
2020-09-20 23:02:35 +0900

    wow

--- 무비앱/movie_app/src/index.js
+++ 무비앱/movie_app/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root') // App.js를 root에 모두 집어넣고싶다는것을 말함
 ); 
+
+
+//이걸로 커밋하자 !!!
